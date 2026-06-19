7966df50980ae008b1b2b15cf8406759f9e16afd
Luiz <luiz.ernica@gmail.com>
2026-06-19 17:57:41 -0300

    feat: First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "temperature-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer =
+    new ConsumerBuilder<Ignore, string>(config)
+    .Build();
+
+consumer.Subscribe("Temperature_topic");
+
+Console.WriteLine("Starting consumer...");
+
+while (true)
+{
+    var result = consumer.Consume();
+
+    Console.WriteLine(
+        $"Received: {result.Message.Value} | " +
+        $"Partition: {result.Partition} | " +
+        $"Offset: {result.Offset}");
+}
